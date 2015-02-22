b7a56faa6a447b587867a90211c0eff9d4c523fd
Markus Westerlind <marwes91@gmail.com>
2015-02-22 15:27:16 +0100

    Added a simple parallel computing function.

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,21 @@
-#![feature(core, unboxed_closures)]
+#![feature(core, os, unboxed_closures)]
+use std::fmt;
 use std::iter::IntoIterator;
+use std::default::Default;
+use std::thread;
+use std::os;
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct Z;
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct Cons<T>(T, usize);
 
-pub trait Shape: Clone {
+pub trait Shape: Clone + Sync {
     fn rank(&self) -> usize;
     fn zero_dim() -> Self;
     fn unit_dim() -> Self;
+    fn add_dim(&self, other: &Self) -> Self;
     fn size(&self) -> usize;
     fn to_index(&self, other: &Self) -> usize;
     fn from_index(&self, other: usize) -> Self;
@@ -22,6 +27,8 @@ impl Shape for Z {
     fn zero_dim() -> Z { Z } 
     #[inline]
     fn unit_dim() -> Z { Z }
+    #[inline]
+    fn add_dim(&self, _: &Z) -> Z { Z }
     #[inline]
     fn size(&self) -> usize { 1 }
     #[inline]
@@ -37,6 +44,10 @@ impl <T: Shape> Shape for Cons<T> {
     #[inline]
     fn unit_dim() -> Cons<T> { Cons(Shape::unit_dim(), 1) }
     #[inline]
+    fn add_dim(&self, other: &Cons<T>) -> Cons<T> {
+        Cons(self.0.add_dim(&other.0), self.1 + other.1)
+    }
+    #[inline]
     fn size(&self) -> usize { self.1 * self.0.size() }
     #[inline]
     fn to_index(&self, index: &Cons<T>) -> usize {
@@ -50,8 +61,8 @@ impl <T: Shape> Shape for Cons<T> {
 }
 
 
-pub trait Source {
-    type Element;
+pub trait Source: Sync {
+    type Element: Send + Sync;
     type Sh: Shape;
 
     fn extent(&self) -> &<Self as Source>::Sh;
@@ -75,6 +86,43 @@ impl <'a, S> Source for &'a S
     }
 }
 
+
+struct Iter<S> {
+    index: usize,
+    source: S
+}
+impl <S> Iterator for Iter<S>
+    where S: Source {
+    type Item = <S as Source>::Element;
+    fn next(&mut self) -> Option<<S as Source>::Element> {
+        if self.index < self.source.extent().size() {
+            let i = self.index;
+            self.index += 1;
+            Some(self.source.linear_index(i))
+        }
+        else {
+            None
+        }
+    }
+}
+fn iter<S>(s: &S) -> Iter<&S>
+    where S: Source {
+    Iter { index: 0, source: s }
+}
+
+struct Fmt<S>(S);
+impl <S> fmt::Display for Fmt<S>
+    where S: Source
+        , <S as Source>::Element: fmt::Display {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        try!(write!(f, "["));
+        for e in iter(&self.0) {
+            try!(write!(f, "{}, ", e));
+        }
+        write!(f, "]")
+    }
+}
+
 pub struct UArray<S, E> {
     shape: S,
     elems: Vec<E>
@@ -92,7 +140,7 @@ impl <S: Shape, E: Clone> UArray<S, E> {
     }
 }
 
-impl <E: Copy, S: Shape> Source for UArray<S, E> {
+impl <E: Copy + Send + Sync, S: Shape> Source for UArray<S, E> {
     type Element = E;
     type Sh = S;
 
@@ -114,9 +162,9 @@ pub struct DArray<S, F>
     f: F
 }
 
-impl <S, F, E> Source for DArray<S, F>
+impl <S, F, E: Send + Sync> Source for DArray<S, F>
     where S: Shape
-        , F: for<'a> Fn(&'a S) -> E {
+        , F: for<'a> Fn(&'a S) -> E + Sync {
     type Element = E;
     type Sh = S;
 
@@ -145,7 +193,8 @@ pub struct MapFn<S, F> {
 }
 
 impl <'a, S, A, B, F> Fn<(&'a <S as Source>::Sh,)> for MapFn<S, F>
-    where F: Fn(A) -> B
+    where A: Send + Sync
+        , F: Fn(A) -> B
         , S: Source<Element=A> {
     type Output = B;
     extern "rust-call" fn call(&self, (sh,): (&<S as Source>::Sh,)) -> B {
@@ -162,12 +211,64 @@ pub fn map<S, F, B>(f: F, array: &S) -> DArray<<S as Source>::Sh, MapFn<&S, F>>
     }
 }
 
+pub struct ExtractFn<S>
+    where S: Source {
+    source: S,
+    start: <S as Source>::Sh,
+}
+
+impl <'a, S> Fn<(&'a <S as Source>::Sh,)> for ExtractFn<S>
+    where S: Source {
+    type Output = <S as Source>::Element;
+    extern "rust-call" fn call(&self, (sh,): (&<S as Source>::Sh,)) -> <S as Source>::Element {
+        let i = self.start.add_dim(sh);
+        self.source.index(&i)
+    }
+}
+
+pub fn extract<S>(start: <S as Source>::Sh, size: <S as Source>::Sh, array: &S) -> DArray<<S as Source>::Sh, ExtractFn<&S>>
+    where S: Source {
+    DArray {
+        shape: size.clone(),
+        f: ExtractFn { source: array, start: start }
+    }
+}
+
 pub fn compute_s<S>(array: &S) -> UArray<<S as Source>::Sh, <S as Source>::Element>
     where S: Source {
     let size = array.extent().size();
     let mut elems = Vec::with_capacity(size);
     for i in 0..size {
         elems.push(array.linear_index(i));
+    }
+    UArray {
+        shape: array.extent().clone(),
+        elems: elems
+    }
+}
+pub fn compute_p<S>(array: &S) -> UArray<<S as Source>::Sh, <S as Source>::Element>
+    where S: Source
+        , <S as Source>::Element: Default + Clone {
+    let size = array.extent().size();
+    let mut elems = vec![Default::default(); size];
+    {
+        let len = elems.len();
+        let slices = os::num_cpus();
+        let slice_len = len / slices;
+        //Save the join guards so that they are 
+        let mut results = Vec::new();
+        for (i, chunk) in elems.chunks_mut(slice_len).enumerate() {
+            let offset = i * slice_len;
+            let x = thread::scoped(move || {
+                for (j, e) in chunk.iter_mut().enumerate() {
+                    *e = array.linear_index(offset + j);
+                }
+            });
+            results.push(x);
+        }
+        for r in results {
+            r.join();
+        }
     }
     UArray {
         shape: array.extent().clone(),
@@ -204,5 +305,22 @@ mod tests {
         let delayed = map(|x| x * 2, &array);
         assert_eq!(delayed.index(&Cons(Cons(Z, 0), 0)), 2);
         assert_eq!(delayed.index(&Cons(Cons(Z, 1), 0)), 6);
+    }
+
+    #[test]
+    fn extract_test() {
+        let m = from_function(SHAPE2X2, |i| SHAPE2X2.to_index(i));
+        let row2 = extract(Cons(Cons(Z, 1), 0), Cons(Cons(Z, 1), 2), &m);
+        assert_eq!(row2.index(&Cons(Cons(Z, 0), 0)), 2);
+        assert_eq!(row2.index(&Cons(Cons(Z, 0), 1)), 3);
+    }
+
+    #[test]
+    fn compute_parallel() {
+        let size = Cons(Cons(Z, 1000), 1000);
+        let m = from_function(size.clone(), |i| size.to_index(i));
+        let indexes = compute_p(&m);
+        let i = Cons(Cons(Z, 200), 21);
+        assert_eq!(indexes.index(&i), indexes.extent().to_index(&i));
     }
 }
